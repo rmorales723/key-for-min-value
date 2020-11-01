42eb5e5e0a17b81e94670c0b10dc00dd351fb780
robert morales <rmoralfit@gmail.com>
2020-11-01 07:51:00 -0500

    commit tree 683fba091f5849f6ec927b1e7d49a235ab3c5767

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
-# Given a hash with numeric values, return the key for the smallest value
+# Given a hash with numeric values, return the key for the smallest valu
 
-def key_for_min_value(name_hash)
+protien_powder = { "isopure" => 28, "purepro" => 22, "complexpro" => 32} 
 
+def key_for_min_value(protien_powder)
+    lowest_value = nil
+        lowest_key = nil
+    protien_powder.collect do | k, v|
+       if lowest_value == nil || v < lowest_value 
+        lowest_value = v
+        lowest_key = k
+       end
+    end
+       lowest_key
 end
